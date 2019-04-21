059c0429e603c20f5e5fe3d8c8006276dd7bb929
Geoff Topley <geoff.topley@gmail.com>
2019-04-21 18:56:32 +0100

    Add ingredients state and have burger component transform ingredients into BurgerIngredient components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,18 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = ({ingredients}) => {
+  const transformedIngredients = Object.keys(ingredients)
+    .map(ingredientKey => {
+      return [...Array(ingredients[ingredientKey])].map((_, i) => {
+        return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>;
+      })
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={"bread-top"}/>
-      <BurgerIngredient type={"cheese"}/>
-      <BurgerIngredient type={"meat"}/>
+      {transformedIngredients}
       <BurgerIngredient type={"bread-bottom"}/>
     </div>
   )
