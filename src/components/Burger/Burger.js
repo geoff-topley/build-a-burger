7dc2ede4feca3123af285db4a19ae72e95d153ee
Geoff Topley <geoff.topley@gmail.com>
2019-04-23 07:38:44 +0100

    Reduce ingredients into flatttened array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
 const burger = ({ingredients}) => {
-  const transformedIngredients = Object.keys(ingredients)
+  let transformedIngredients = Object.keys(ingredients)
     .map(ingredientKey => {
       return [...Array(ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>;
       })
-    });
+    })
+    .reduce((prev, current) => {
+      return prev.concat(current)
+    }, []);
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={classes.Burger}>
