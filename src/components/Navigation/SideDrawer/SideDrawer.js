fac0460c2409c35bf5a9dc132799034f847dbd51
Geoff Topley <geoff.topley@gmail.com>
2019-05-20 22:08:29 +0100

    Re-use backdrop for sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
-const sideDrawer = (props) => {
+const sideDrawer = ({open, sideDrawerClosed}) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if(open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo/>
+    <Aux>
+      <Backdrop
+        show={open}
+        sideDrawerClosed={sideDrawerClosed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+      <Backdrop/>
+    </Aux>
   );
 };
 
--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Backdrop.css';
 
 const backdrop = (props) => (
-  props.show ? <div className={classes.Backdrop} onClick={props.closeModal}>
+  props.show ? <div className={classes.Backdrop} onClick={props.sideDrawerClosed}>
 
   </div> : null
 );
