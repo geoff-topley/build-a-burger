a0a7b2d04fd224e8996b40ae4ded3f62cad6b81f
Geoff Topley <geoff.topley@gmail.com>
2019-05-30 22:05:42 +0100

    Only render OrderSummary if Modal is shown - improves performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ({ingredients, purchaseCancelled, purchaseContinued, totalPrice}) => {
-  const ingredientSummary = Object.keys(ingredients)
-    .map(ingredientKey => {
+class OrderSummary extends Component {
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(ingredientKey => {
         return (
           <li key={ingredientKey}>
-            <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: {ingredients[ingredientKey]}
+            <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
           </li>);
       });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: £{totalPrice}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button buttonType="Danger" clicked={purchaseCancelled}>CANCEL</Button>
-      <Button buttonType="Success" clicked={purchaseContinued}>CONTINUE</Button>
-    </Aux>
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: £{this.props.totalPrice}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button buttonType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
     )
-};
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
