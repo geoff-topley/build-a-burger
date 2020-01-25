5e2e734fd5aa7757d54ed6a92807211c6fb0f5be
Geoff Topley <geoff.topley@gmail.com>
2020-01-25 22:19:07 +0000

    Post data to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../../src/axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 1,
@@ -102,7 +103,22 @@ class BurgerBuilder extends Component{
 
 
   purchaseContinueHandler = () => {
-    alert("You will continue!")
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Geoff",
+        address: {
+          street: "1 Test Lane",
+          postCode: "A187 6TH"
+        }
+      },
+      deliveryMethod: "fastest"
+    };
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
 
